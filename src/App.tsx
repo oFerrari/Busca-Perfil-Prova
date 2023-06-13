42c252df29d27821e334321efb9291301f9f42fb
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-06-13 08:46:50 -0300

    🎨 Finalizando Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { Box, Button, TextField } from "@mui/material"
+import { CardSearch } from "./components/Card"
 
 
 function App() {
 
   return (
     <>
-      oii
+    <Box sx={{
+      display: "flex",
+      justifyContent:'center',
+      alignItems:'center',
+      flexDirection:'column',
+      rowGap:'50px'
+    }}>
+      
+    <TextField
+          id="filled-search"
+          label="Search field"
+          type="search"
+          variant="filled"
+      />
+
+      <Button variant="outlined">Primary</Button>
+
+    <CardSearch />
+    </Box>
+
+      
     </>
   )
 }
