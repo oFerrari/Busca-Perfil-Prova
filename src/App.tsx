51c566349c3f49453ac5f29c5685b678d98369bc
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-06-14 21:09:53 -0400

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Avatar, Box, Button, Grid, TextField, Typography } from "@mui/material"
+import { Avatar, Box, Button, Grid, TextField, Typography } from "@mui/material";
 import { ChangeEvent, useEffect, useState } from "react";
 import { getUser } from "./services/api";
 
 export type User = {
-  avatar_url: string;
-  name: string;
-  html_url: string;
+  avatar: string;
+  first_name: string;
+  last_name: string;
+  email: string;
 };
 
 function App() {
-  const [user, setUser] = useState<User>({avatar_url:"",html_url:"",name:""});
+  const [user, setUser] = useState<User>({ avatar: "", first_name: "", last_name: "", email: "" });
   const [input, setInput] = useState("");
 
   const handleClick = async () => {
@@ -22,15 +23,12 @@ function App() {
   };
 
   useEffect(() => {
-    // Função para buscar os dados do usuário ao montar o componente
-    async function fetchUser() {
-      if (input) {
-        const userData = await getUser(input);
-        setUser(userData);
-      }
+    async function listUser() {
+      const userData = await getUser(input);
+      setUser(userData);
     }
 
-    fetchUser();
+    listUser();
   }, []);
 
   return (
@@ -44,6 +42,7 @@ function App() {
           rowGap: "50px",
         }}
       >
+        {/* Campo de texto para pesquisa */}
         <TextField
           value={input}
           label="Search field"
@@ -52,38 +51,32 @@ function App() {
           onChange={handleChange}
         />
 
+        {/* Botão para iniciar a busca */}
         <Button variant="outlined" onClick={handleClick}>
           Primary
         </Button>
 
-        
-        
-        <Grid sx={{
-        width: '600px',
-        background: 'purple',
-        border: '1px solid black',
-        borderRadius: '5px',
-        justifyContent: 'center',
-        color:'white' // Adicionando alinhamento horizontal centralizado
-      }}
-    >
-        <Avatar src={user.avatar_url}/>
-      <Typography>
-      {user.name}
-      </Typography>
-      <Typography>
-      <a href={user.html_url}>Link do Repositório</a>
-      </Typography>
-    </Grid>
+        {/* Grid para exibir os detalhes do usuário */}
+        <Grid
+          sx={{
+            width: "600px",
+            background: "purple",
+            border: "1px solid black",
+            borderRadius: "5px",
+            justifyContent: "center",
+            color: "white",
+          }}
+        >
+          {/* Avatar do usuário */}
+          <Avatar src={user.avatar} />
 
-      
-    
+          {/* Nome do usuário */}
+          <Typography>{`${user.first_name} ${user.last_name}`}</Typography>
 
-
-
+          {/* Email do usuário */}
+          <Typography>{user.email}</Typography>
+        </Grid>
       </Box>
-
-      
     </>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function getUser(username:string) {
+export async function getUser() {
     
-      const response = await axios.get(`https://api.github.com/users/${username}`);
+      const response = await axios.get(`https://reqres.in/api/users?page=1`);
       return response.data
 }
